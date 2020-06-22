4d62f055a203df91050a5f01e73b6d36f084d8e8
= <=>
2020-06-22 09:39:23 +0100

    remove npm module
    remove npm module from config file

---
+++ build/precache-manifest.47377a64c2f2e4adb66436fc871f3203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad8863151c747e25ca444b60b578c8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f0a24f214609348905b",
+    "url": "/static/css/main.57629752.chunk.css"
+  },
+  {
+    "revision": "397399ced03f69d40624",
+    "url": "/static/js/2.c73d964f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c73d964f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f0a24f214609348905b",
+    "url": "/static/js/main.8b61d962.chunk.js"
+  },
+  {
+    "revision": "4cc4b4813bd8d251ff58",
+    "url": "/static/js/runtime-main.0c3af75d.js"
+  }
+]);
